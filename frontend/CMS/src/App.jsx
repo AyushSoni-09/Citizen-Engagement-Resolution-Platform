9fd16d3cc5c0060e190209fffec4f781fc1a8759
Ayush Soni <113293849+AyushSoni-09@users.noreply.github.com>
2025-12-16 15:46:56 +0000

    resolveed a error

--- frontend/CMS/src/App.jsx
+++ frontend/CMS/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
-import Login from './Components/Login'
+import Login from '../Components/Login'
 import './App.css'
 
 function App() {
